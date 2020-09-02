8bed0e0d90d1ba601a4cf044464a0b9d311f0ea5
John Grib <johngrib82@gmail.com>
2020-09-02 23:25:34 +0900

    메모 전략을 사용하는 계산기를 추가하라

--- src/test/java/meenakshi/_2_2/MinimumTicketPriceCalculatorTest.java
+++ src/test/java/meenakshi/_2_2/MinimumTicketPriceCalculatorTest.java
@@ -30,15 +30,63 @@ class MinimumTicketPriceCalculatorTest {
         new int[] {-1, -1, -1, 0},
     };
 
-    private int subject(StationFact fact) {
-      final var calculator = new RecursiveMinimumTicketPriceCalculator();
-      calculator.setCost(cost);
-      return calculator.minCost(fact.start, fact.destination);
+    @Nested
+    @DisplayName("메모 전략을 사용하는 계산기는")
+    class ContextWithMemoizeCalculator {
+      private int subject(StationFact fact) {
+        final var calculator = new MemoizeMinimumTicketPriceCalculator();
+        calculator.setCost(cost);
+        return calculator.minCost(fact.start, fact.destination);
+      }
+
+      @Nested
+      @DisplayName("출발역과 도착역이 같다면")
+      class ContextWithSameStations {
+        final List<StationFact> givenFacts = List.of(
+            StationFact.of(0, 0, 0),
+            StationFact.of(1, 1, 0),
+            StationFact.of(2, 2, 0),
+            StationFact.of(3, 3, 0)
+        );
+
+        @Test
+        @DisplayName("0원을 리턴한다")
+        void it_returns_min_price() {
+          for (var fact : givenFacts) {
+            Assertions.assertEquals(0, subject(fact));
+          }
+        }
+      }
+
+      @Nested
+      @DisplayName("같지 않은 출발역과 도착역이 주어지면")
+      class ContextWithSameStationss {
+        final List<StationFact> givenFacts = List.of(
+            StationFact.of(0, 1, 10),
+            StationFact.of(0, 2, 10 + 35),
+            StationFact.of(0, 3, 10 + 50),
+            StationFact.of(1, 2, 35)
+        );
+
+        @Test
+        @DisplayName("최소 비용을 계산해 리턴한다")
+        void it_returns_min_price() {
+          for (var fact : givenFacts) {
+            Assertions.assertEquals(fact.expectPrice, subject(fact));
+          }
+        }
+      }
     }
 
     @Nested
     @DisplayName("재귀적 계산기는")
     class ContextWithRecursiveCalculator {
+      private int subject(StationFact fact) {
+        final var calculator = new RecursiveMinimumTicketPriceCalculator();
+        calculator.setCost(cost);
+        return calculator.minCost(fact.start, fact.destination);
+      }
+
       @Nested
       @DisplayName("출발역과 도착역이 같다면")
       class ContextWithSameStations {
